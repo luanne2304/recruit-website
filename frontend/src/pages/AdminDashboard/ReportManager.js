1d8b3c4c2eb813be3a20c5696a2a27de46df0f8a
HoangTran1807 <tranhuuhoang692@gmail.com>
2024-04-03 11:09:38 +0700

    reportManagerUpdate

---
+++ frontend/src/pages/AdminDashboard/ReportManager.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Stack } from "@mui/material";
+import ReportItem from "./components/ReportItem";
+
+export default function ReportManager() {
+    return (
+        <div>
+            <Stack>
+                <ReportItem />
+                <ReportItem />
+                <ReportItem />
+                <ReportItem />
+                <ReportItem />
+                <ReportItem />
+                <ReportItem />
+                <ReportItem />
+                <ReportItem />
+                <ReportItem />
+                <ReportItem />
+                <ReportItem />
+            </Stack>
+            
+        </div>
+    )
+}
+
